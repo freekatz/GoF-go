af9830eded08daf1624822fd89e72579f7d6126b
1uvu <zjh98@vip.qq.com>
2021-07-03 19:47:57 +0800

    update code commit

--- builder/packet.go
+++ builder/packet.go
@@ -3,10 +3,9 @@ package builder
 type Packet interface {
 	// 定义 packet 方法
 	Payload() []byte // 获取下一层数据包, 如 IPv4.Payload() 会返回 Ethernet 的数据包
-
-	Header() []byte // 输出 header
-	Bytes() []byte  // 返回整个数据包
-	Copy() Packet
+	Header() []byte  // 输出 header
+	Bytes() []byte   // 返回整个数据包
+	Copy() Packet    // return a deep copy
 }
 
 type Header interface {
